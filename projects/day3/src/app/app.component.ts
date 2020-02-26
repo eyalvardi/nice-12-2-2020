f0da945f6edc9fe3f5a2ae03068af14500ad6d0d
eyalvardi <evardi@gmail.com>
2020-02-26 10:46:36 +0200

    day3 - start - forms

--- projects/day3/src/app/app.component.ts
+++ projects/day3/src/app/app.component.ts
@@ -1,32 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
+import { tap } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
+   <div>
+     <h2>Day 3 NICE !!!!</h2>
+     <div [formGroup]="myForm">
+       name  : <input type="text" formControlName="name">
+       age   : <input  formControlName="age">
+       email : <input  formControlName="email">
+       cards:<br>
+       <ng-container *ngFor="let fc of cardsFc.controls;last as l;index as i">
+         <input [formControl]="fc">
+         <button (click)="removeCard(i)">-</button>
+         <button *ngIf="l" (click)="addCard()">+</button>
+         <br>
+       </ng-container>       
+       <pre>
+         {{myForm.value | json }}
+       </pre>
+     </div>
+   </div>
+
     
   `,
-  styles: []
+  styles: [`
+    .ng-invalid{
+      border-right: 2px solid red;
+    }
+    .ng-valid{
+      border-right: 2px solid green;
+    }
+  `]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'day3';
+  nameFc = new FormControl('eyal',[Validators.required],[]);
+  myForm = new FormGroup({
+    name : this.nameFc,
+    age  : new FormControl('35'),
+    email: new FormControl('ee@eee.com',[Validators.email]),
+    cards : new FormArray([
+      new FormControl('abc')
+    ])
+  },[Validators.nullValidator]);
+
+  get cardsFc() : FormArray {
+    return this.myForm.get('cards') as FormArray;
+  }
+
+  ngOnInit(): void {
+    this.myForm.valueChanges
+        .pipe(
+            tap( value => console.dir(value))
+        )
+        .subscribe()
+  }
+
+  addCard() {
+    this.cardsFc.push(new FormControl());
+  }
+
+  removeCard(i: number) {
+    this.cardsFc.removeAt(i);
+  }
 }

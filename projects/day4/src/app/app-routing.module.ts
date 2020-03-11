49d3d7c05aad8963a3960ff62d350319acccd50b
eyalvardi <evardi@gmail.com>
2020-03-11 10:07:59 +0200

    day4 - routing 1

--- projects/day4/src/app/app-routing.module.ts
+++ projects/day4/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { ErrorComponent } from "./error/error.component";
+import { UsersComponent } from "./users/components/users.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'users', component: UsersComponent},
+
+    {path: '**' , component : ErrorComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- projects/day4/src/app/app.module.ts
+++ projects/day4/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { UsersModule } from "./users/users.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        ErrorComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule,
+        AppRoutingModule,
+        UsersModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
